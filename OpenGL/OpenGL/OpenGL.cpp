6e005a3523db83fff0459f477a1ddf272fcc0f27
Renae Aurisch <Renae.Aurisch@hotmail.com>
2019-03-13 12:33:54 +1030

    3D grid created

--- OpenGL/OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL/OpenGL.cpp
@@ -2,11 +2,79 @@
 //
 
 #include "pch.h"
+#include "gl_core_4_4.h"
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include "Gizmos.h"
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
+using namespace aie;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	if (glfwInit() == false)
+		return -1;
+	GLFWwindow* window = glfwCreateWindow(1280, 720,
+		"Computer Graphics",
+		nullptr, nullptr);
+	if (window == nullptr) {
+		glfwTerminate();
+		return -2;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+	auto major = ogl_GetMajorVersion();
+	auto minor = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", major, minor);
+
+	glClearColor(0.25f, 0.25f, 0.25f, 1);
+
+	Gizmos::create(65336, 65336, 256, 256);
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f,
+		16 / 9.f, 0.1f, 1000.f);
+
+	while (glfwWindowShouldClose(window) == false &&
+		glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+		// our game logic and update code goes here!
+		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST); // enables the depth buffer
+
+		Gizmos::clear();
+		Gizmos::addTransform(glm::mat4(1));
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+		for (int i = 0; i < 21; ++i) {
+			Gizmos::addLine(vec3(-10 + i, 0, 10),
+				vec3(-10 + i, 0, -10),
+				i == 10 ? white : black);
+			Gizmos::addLine(vec3(10, 0, -10 + i),
+				vec3(-10, 0, -10 + i),
+				i == 10 ? white : black);
+		}
+		Gizmos::draw(projection * view);
+
+		// so does our render code!
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	Gizmos::destroy();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
